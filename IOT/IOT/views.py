62563c8299fb4e222be624ccb0c702e7ab282960
onlyme275 <onlyme12312322123@gmail.com>
2026-01-12 20:32:43 +0545

    making frontend

--- IOT/IOT/views.py
+++ IOT/IOT/views.py
@@ -1,15 +1,11 @@
 from django.http import JsonResponse
-import json
-import os
-
-DATA_FILE = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "latest_data.json")
+from esp.mqtt_service import mqtt_service
 
 def get_water_level(request):
-    if os.path.exists(DATA_FILE):
-        with open(DATA_FILE, 'r') as f:
-            data = json.load(f)
-            return JsonResponse(data)
-    return JsonResponse({"water": 0, "raw": 0})
+    return JsonResponse({
+        "water": mqtt_service.water_level,
+        "raw": 0
+    })
 
 
 
